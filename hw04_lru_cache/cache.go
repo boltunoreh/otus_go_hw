f0c8572dc4b0354554029cd1679b657df743d5e7
Pavel Ivanov <pavel.o.ivanov@vseinstrumenti.ru>
2022-04-14 13:34:45 +0300

    implement cache

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -9,11 +9,47 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+}
+
+func (cache *lruCache) Set(key Key, value interface{}) bool {
+	_, itemExists := cache.Get(key)
+
+	if itemExists == false && cache.queue.Len() == cache.capacity {
+		backItem := cache.queue.Back()
+		cache.queue.Remove(backItem)
+	}
+
+	var listItem *ListItem
+	if itemExists {
+		listItem = cache.items[key]
+		listItem.Value = value
+		cache.queue.MoveToFront(listItem)
+	} else {
+		listItem = cache.queue.PushFront(value)
+	}
+
+	cache.items[key] = listItem
+
+	return itemExists
+}
+
+func (cache *lruCache) Get(key Key) (interface{}, bool) {
+	item, ok := cache.items[key]
+
+	if ok {
+		cache.queue.MoveToFront(item)
+		return item.Value, ok
+	}
+
+	return nil, ok
+}
+
+func (cache *lruCache) Clear() {
+	cache.queue = NewList()
+	cache.items = make(map[Key]*ListItem, cache.capacity)
 }
 
 type cacheItem struct {
